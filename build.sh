43118535d293ba82a811e8237b1bae67f040cb37
a.sinelnikov <a.sinelnikov@inspector-cloud.com>
2026-05-24 23:28:16 +0300

    fix build.sh

--- build.sh
+++ build.sh
@@ -1,10 +1,6 @@
 #!/usr/bin/env bash
 
-source .env
 curl -LsSf https://astral.sh/uv/install.sh | sh
+source .env
 make install
-
-DB_NAME=${DATABASE_URL##*/}
-echo "Creating database '$DB_NAME'..."
-psql -d "$DATABASE_URL" -c "CREATE DATABASE $DB_NAME;"
-psql -d "$DATABASE_URL" -f database.sql
+psql -a -d $DATABASE_URL -f database.sql
